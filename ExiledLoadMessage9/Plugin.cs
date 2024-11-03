d211cf937920ec30b785b409adf8279439438521
AngelSeraphim <server@praniksl.ru>
2024-11-03 13:43:18 +0500

    9.0.0

---
+++ ExiledLoadMessage9/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+
+namespace ExiledLoadMessage
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "angelseraphim.";
+        public override string Name => "ExiledLoadMessage";
+        public override string Prefix => "ExiledLoadMessage";
+
+        public static Plugin plugin;
+        public static Harmony harmony;
+        public override void OnEnabled()
+        {
+            plugin = this;
+            harmony = new Harmony("ExiledLoadMessage");
+            harmony.PatchAll();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            plugin = null;
+            harmony.UnpatchAll();
+            base.OnDisabled();
+        }
+    }
+}
