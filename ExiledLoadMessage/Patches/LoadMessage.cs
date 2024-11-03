27698758170b0b0ef0784cf266bd7ea04e096560
AngelSeraphim <server@praniksl.ru>
2024-11-03 13:39:05 +0500

    Добавьте файлы проекта.

---
+++ ExiledLoadMessage/Patches/LoadMessage.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Exiled.Loader.Features;
+
+namespace ExiledLoadMessage.Patches
+{
+    [HarmonyPatch(typeof(LoaderMessages), nameof(LoaderMessages.GetMessage))]
+    internal class LoadMessage
+    {
+        static void Postfix(ref string __result) => __result = Plugin.plugin.Config.LoadText;
+    }
+}
